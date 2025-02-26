571ad2c7b796604d2dee5561aee22d9d6e02da07
igl112 <igl112@inlumine.ual.es>
2025-02-26 18:27:14 +0100

    Componentes: index, servicios, equipo, contacto, parallax, footer y header

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { EquipoComponent } from './components/equipo/equipo.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactoComponent } from "./components/contacto/contacto.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IndexComponent, EquipoComponent, ServiciosComponent],
+  imports: [RouterOutlet, IndexComponent, EquipoComponent, ServiciosComponent, ContactoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
--- angular/src/app/components/index/index.component.ts
+++ angular/src/app/components/index/index.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ParallaxComponent } from '../parallax/parallax.component';
 
 @Component({
   selector: 'app-index',
-  imports: [], //importar footer, header y fondo
+  imports: [HeaderComponent, FooterComponent, ParallaxComponent], //importar footer, header y fondo
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
