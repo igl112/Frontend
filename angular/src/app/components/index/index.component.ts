6e6e904205caf1f7cce5acb6ac4a9165de85a8c5
igl112 <igl112@inlumine.ual.es>
2025-02-26 12:17:27 +0100

    Creacion carpeta assests y components

--- angular/src/app/components/index/index.component.ts
+++ angular/src/app/components/index/index.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [], //importar footer, header y fondo
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
