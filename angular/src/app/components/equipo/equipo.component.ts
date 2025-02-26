9f429888a0899d7c29e927197390e11dcc8920b3
Franmi <Franmii66@hotmail.com>
2025-02-26 20:59:58 +0100

    Creacion de los componentes, acerca de y comentarios, y añadiendo los componentes que faltaban en otras paginas

--- angular/src/app/components/equipo/equipo.component.ts
+++ angular/src/app/components/equipo/equipo.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent} from '../header/header.component';
+import { FooterComponent} from '../footer/footer.component';
+import { ParallaxComponent} from '../parallax/parallax.component';
 
 @Component({
   selector: 'app-equipo',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, ParallaxComponent],
   templateUrl: './equipo.component.html',
   styleUrl: './equipo.component.css'
 })
