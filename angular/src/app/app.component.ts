ecc1f16b092706fa006af70eadd1dbfc51af8f65
igl112 <igl112@inlumine.ual.es>
2025-02-26 18:21:37 +0100

    Componente Contacto (no se ve el mapa)

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { EquipoComponent } from './components/equipo/equipo.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactoComponent } from "./components/contacto/contacto.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IndexComponent, EquipoComponent, ServiciosComponent],
+  imports: [RouterOutlet, IndexComponent, EquipoComponent, ServiciosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
