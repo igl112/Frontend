f4132fa4c8bd53ee64de6a33ba41c62f32e1eebd
igl112 <igl112@inlumine.ual.es>
2025-02-26 17:10:39 +0100

    Imagenes en componentes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { EquipoComponent } from './components/equipo/equipo.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IndexComponent, EquipoComponent],
+  imports: [RouterOutlet, IndexComponent, EquipoComponent, ServiciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
