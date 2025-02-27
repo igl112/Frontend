b992377f81813a7be73743d5a48f1b924c71263c
LuzSerranoDiaz <diaz03luz@gmail.com>
2025-02-27 10:10:48 +0100

    merge devLuz

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
-import { EquipoComponent } from './components/equipo/equipo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IndexComponent, EquipoComponent],
+  imports: [RouterOutlet, IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
